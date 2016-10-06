f3130ff2702d67c8144d1d0ca069df267c9246bf
Maciek Kaminski <maciej.michal.kaminski@gmail.com>
2016-10-06 20:46:30 +0200

    Update RestServiceImpl.java

--- tasklist-model/src/main/java/org/apache/aries/jpa/example/tasklist/model/RestServiceImpl.java
+++ tasklist-model/src/main/java/org/apache/aries/jpa/example/tasklist/model/RestServiceImpl.java
@@ -10,24 +10,17 @@ import org.apache.aries.jpa.example.tasklist.ds.impl.TaskService;
 import pl.orange.isep.model.service.Service;
 
 public class RestServiceImpl implements RestService {
-	
-//	TaskService srv;
-	
+
+
 	  @PersistenceContext(unitName = "tasklist")
 	  EntityManager em;
 	
 	  @Override
 		public List<Service> getAll() {
-//			logger.info("JPA TEMPLATE "+getJpa());
-//			logger.info("JPA TEMPLATE "+em);
-//			return getJpa().txExpr(new EmFunction<List<Service>>() {
-//	    		@Override
-//	    		public List<Service> apply(EntityManager em) {
+
 	    			List<Service> resultList = em.createQuery("select s from Service s", Service.class).getResultList();
-//	    			logger.info("JPA TEMPLATE returned "+resultList);
+
 	    			return resultList;
-//	    		}
-//	    	});
 		}
 
 }
